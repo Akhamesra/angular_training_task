65704db59541e9fc364b4d16b50c6673e245f03c
Hardik-2019 <58595142+Hardik-2019@users.noreply.github.com>
2023-02-02 11:18:51 +0530

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,14 @@
+create table emp(
+	empcode integer,
+	name varchar(50),
+	email varchar(50),
+	designation varchar(50),
+	location varchar(50)
+);
+insert into emp values(1,'Hardik','hardikgarg01@gmail.com','Intern','Pune');
+insert into emp values(2,'Amit','amit01@gmail.com','Intern','Pune');
+insert into emp values(3,'Vishaal','vishaal01@gmail.com','Intern','Pune');
+insert into emp values(4,'Sarwesh','sarwesh01@gmail.com','Intern','Pune');
+insert into emp values(5,'Pranav','pranav01@gmail.com','Intern','Pune');
+select * from emp;
+
